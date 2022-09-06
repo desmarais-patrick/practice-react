5f22082f30f56c065e541d28f72b8e36ca69c538
Patrick Desmarais <patrick.desm@gmail.com>
2022-09-06 15:27:40 -0400

    Experiment with asset resource module (WebPack)

--- signup-form-exercise/fe-react-controlled-form/webpack.config.js
+++ signup-form-exercise/fe-react-controlled-form/webpack.config.js
@@ -1,8 +1,16 @@
+const path = require('path');
+
 module.exports = {
+    mode: 'development',
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    },
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader",
